24922e765199f25495511c4a362ce7cce34e335e
aniespica <aniespica@gmail.com>
2015-11-17 14:11:38 -0500

    files like update images

--- app/routers/index.js
+++ app/routers/index.js
@@ -5,7 +5,7 @@ module.exports = function (router, mysql, pool) {
 	/* Functions INSERT, UPDATE, DELETE records - SF to RDS */
 	router.post('/insert/', function (req, res) { sf.insert(req, res, pool);});
 
-	router.get('/*', function (req, res) {
+	router.get('/', function (req, res) {
 		res.send('SF insert test!');
 		res.end();
 	})
