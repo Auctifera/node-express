b684b4a27faaa29346d9d851c17045b08346aa19
aniespica <aniespica@gmail.com>
2015-11-17 13:48:22 -0500

    final checkin

--- app/routers/index.js
+++ app/routers/index.js
@@ -4,6 +4,10 @@ module.exports = function (router, mysql, pool) {
 	
 	/* Functions INSERT, UPDATE, DELETE records - SF to RDS */
 	router.post('/insert/', function (req, res) { sf.insert(req, res, pool);});
+
+	router.get('/', function (req, res) {
+		res.send('SF insert test!');
+	})
 	// router.put('/update/', function (req, res) {});
 	// router.delete('/delete/', function (req, res) {});
 }
